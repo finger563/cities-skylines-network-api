342444bb89fa51fbf7e16d80e9a84e7026e03de8
William Emfinger <waemfinger@gmail.com>
2016-06-28 10:38:42 -0500

    Refactored code to bring interface and implementation out of main file.  Also removed no longer needed UDP code.

---
+++ NetworkAPI/INetwork.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace NetworkAPI
+{
+    [ServiceContract]
+    public interface INetwork
+    {
+        [WebGet(UriTemplate = "assemblies/{assemblyName}", BodyStyle = WebMessageBodyStyle.Bare, ResponseFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        List<string> GetAssemblyTypes(string assemblyName);
+
+        [WebGet(UriTemplate = "managers", BodyStyle = WebMessageBodyStyle.Bare, ResponseFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        List<string> GetManagers();
+
+        [WebGet(UriTemplate = "managers/{managername}", BodyStyle = WebMessageBodyStyle.Bare, ResponseFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        List<string> GetManagerTypes(string managername);
+
+        [WebGet(UriTemplate = "managers/{managername}/{type}", BodyStyle = WebMessageBodyStyle.Bare, ResponseFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        List<string> GetManagerProperties(string managername, string type);
+
+        [WebGet(UriTemplate = "managers/{managername}/{type}/{propertyname}", BodyStyle = WebMessageBodyStyle.Bare, ResponseFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        string GetManagerProperty(string managername, string type, string propertyname);
+    }
+}
---
+++ NetworkAPI/Network.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Collections;
+using System.Reflection;
+
+using ICities;
+using UnityEngine;
+using ColossalFramework.UI;
+using ColossalFramework.Plugins;
+
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+using System.Web.Script.Serialization;
+
+namespace NetworkAPI
+{
+    public class Network : INetwork
+    {
+        JavaScriptSerializer serializer;
+
+        public Network()
+        {
+            serializer = new JavaScriptSerializer();
+        }
+
+        public List<string> GetAssemblyTypes(string assemblyName)
+        {
+            List<string> types = new List<string>();
+            Assembly assembly = Assembly.Load(assemblyName);
+            types = assembly.GetTypes().Select(x => x.Name).ToList<string>();
+            return types;
+        }
+
+        public List<string> GetManagers()
+        {
+            List<string> managers = new List<string>();
+            try
+            {
+                Assembly assembly = Assembly.Load("Assembly-CSharp");
+                managers = assembly.GetTypes()
+                    .Where(x => x.Name.IndexOf("Manager") > -1)
+                    .Select(x => x.Name).ToList<string>();
+            }
+            catch (Exception e)
+            {
+                managers.Add(e.Message);
+            }
+            return managers;
+        }
+
+        public List<string> GetManagerTypes(string managername)
+        {
+            List<string> types = new List<string>();
+            types.Add("members");
+            types.Add("methods");
+            types.Add("properties");
+            types.Add("fields");
+            types.Add("events");
+            types.Add("nestedTypes");
+            return types;
+        }
+
+        public List<string> GetManagerProperties(string managername, string type)
+        {
+            List<string> properties = new List<string>();
+            try
+            {
+                Assembly assembly = Assembly.Load("Assembly-CSharp");
+                Type t = assembly.GetType(managername);
+                if (type == "properties")
+                {
+                    properties = t.GetProperties().Select(x => x.Name).ToList<string>();
+                }
+                else if (type == "methods")
+                {
+                    properties = t.GetMethods().Select(x => x.Name).ToList<string>();
+                }
+                else if (type == "members")
+                {
+                    properties = t.GetMembers().Select(x => x.Name).ToList<string>();
+                }
+                else if (type == "fields")
+                {
+                    properties = t.GetFields().Select(x => x.Name).ToList<string>();
+                }
+                else if (type == "events")
+                {
+                    properties = t.GetEvents().Select(x => x.Name).ToList<string>();
+                }
+                else if (type == "nestedTypes")
+                {
+                    properties = t.GetNestedTypes().Select(x => x.Name).ToList<string>();
+                }
+            }
+            catch (Exception e)
+            {
+                properties.Add(e.Message);
+            }
+            return properties;
+        }
+
+        public Type getAssemblyType(string assemblyName, string typeName)
+        {
+            return Assembly.Load(assemblyName).GetType(typeName);
+        }
+
+        public object getInstance(string assemblyName, string typeName)
+        {
+            // get the instance of the manager here:
+            Type t = getAssemblyType( assemblyName, typeName);
+            PropertyInfo instancePropInfo = (PropertyInfo)t.GetMember("instance")[0];
+            MethodInfo instanceMethodInfo = instancePropInfo.GetAccessors()[0];
+            return instanceMethodInfo.Invoke(null, null);
+        }
+
+        public object getPropertyValue(string managername, string name)
+        {
+            object retObj;
+            object manager = getInstance("Assembly-CSharp", managername);
+            Type t = getAssemblyType("Assembly-CSharp", managername);
+            MethodInfo mi = t.GetProperty(name).GetGetMethod();
+            retObj = mi.Invoke(manager, null);
+            return retObj;
+        }
+
+        public string GetManagerProperty(string managername, string type, string propertyname)
+        {
+            string returnString = "";
+            try
+            {
+                Type t = getAssemblyType("Assembly-CSharp", managername);
+                object manager = getInstance("Assembly-CSharp", managername);
+
+                if (type == "properties")
+                {
+                    returnString += getPropertyValue(managername, propertyname);
+                }
+                else if (type == "methods")
+                {
+                    MemberInfo[] m = t.GetMember(propertyname);
+                    foreach (ParameterInfo pi in ((MethodInfo) m[0]).GetParameters())
+                    {
+                        returnString += string.Format("{0} {1}, ", pi.ParameterType, pi.Name);
+                    }
+                }
+                else if (type == "members")
+                {
+                    MemberInfo[] pa = t.GetMember(propertyname);
+                    foreach (var p in pa)
+                    {
+                        object[] attrs = p.GetCustomAttributes(false);
+                        foreach (object o in attrs)
+                        {
+                            returnString += o.ToString() + ' ';
+                        }
+                        if (p.MemberType == MemberTypes.Method)
+                        {
+                            foreach (ParameterInfo pi in ((MethodInfo) p).GetParameters())
+                            {
+                                returnString += string.Format("{0} {1}, ", pi.ParameterType, pi.Name);
+                            }
+                        }
+                        if (p.MemberType == MemberTypes.Property)
+                        {
+                            returnString += getPropertyValue(managername, propertyname);
+                        }
+                    }
+                }
+                else if (type == "fields")
+                {
+                    FieldInfo p = t.GetField(propertyname);
+                    returnString += p.GetValue(manager);
+                }
+            }
+            catch (Exception e)
+            {
+                returnString += "ERROR: " + e.Message;
+            }
+            return returnString;
+        }
+    }
+}
